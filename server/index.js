e1660d8b830056eadfd84c5673edd73433fd7c72
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2021-06-16 21:14:22 +0300

    add userSchema. add .gitignore

--- server/index.js
+++ server/index.js
@@ -1,14 +1,28 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const URL =
+  "mongodb+srv://root:root@cluster0.llw9u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const PORT = 5000;
-const server = express();
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
 const start = async () => {
-  server.listen(PORT, () => {
+  try {
+    await mongoose.connect(URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`STAAART - ${PORT}`);
-  });
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 start();
